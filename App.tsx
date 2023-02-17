12ea2eebe753ebe90285adc37a914eafc8c7a87d
huseynmv <huseynmv@yahoo.com>
2023-02-17 13:45:03 +0400

    firebase android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react';
 import firebase from '@react-native-firebase/app';
 import messaging from '@react-native-firebase/messaging';
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
   useEffect(() => {
+    SplashScreen.hide();
     messaging()
-      .getToken(firebase.app(''))
+      .getToken(firebase.app())
       .then(x => console.log(x))
       .catch(e => console.log(e));
   }, []);
